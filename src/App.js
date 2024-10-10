b74ad3a7db4dd006fe9bf76ffd30b888db22faeb
Matteo F <matteof19@icloud.com>
2024-10-10 15:34:08 +0200

    Body2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+//import './stylsheets/topbar.css';
 
 function App() {
   return (
       <>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+
         </header>
         <body>
         <div className="header">
           <div className="left-section">
-            <img src={logo} className="App-logo" alt="logo"/>
+            <img src={logo} className="hamburger-menu" alt="logo"/>
           </div>
           <div className="middle-section">
-              <input/>
-              <input/>
+              <input className= "search-bar" type ="text" placeholder="Suchen" />
           </div>
           <div className="right-section">
 
