7c4e3c1947aa6ca5165d3aae68262d231ca4b2dc
Matteo F <matteof19@icloud.com>
2024-10-10 15:43:55 +0200

    Body3

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-//import './stylsheets/topbar.css';
+
+//import './stylesheets/topbar.css';
 
 function App() {
-  return (
-      <>
-        <header>
+    return (
+        <>
+            <header>
 
-        </header>
-        <body>
-        <div className="header">
-          <div className="left-section">
-            <img src={logo} className="hamburger-menu" alt="logo"/>
-          </div>
-          <div className="middle-section">
-              <input className= "search-bar" type ="text" placeholder="Suchen" />
-          </div>
-          <div className="right-section">
-
-          </div>
-        </div>
-        </body>
-      </>
-  );
+            </header>
+            <body>
+            <div className="header">
+                <div className="left-section">
+                    <img src={logo} className="hamburger-menu" alt="logo"/>
+                    <img src={logo} className="NARKO-logo" alt="logo"/>
+                </div>
+                <div className="middle-section">
+                <input className="search-bar" type="text" placeholder="Suchen"/>
+                    <button className="search-button">
+                        <img src={logo} className="search-icon" alt="logo"/>
+                    </button>
+                </div>
+                <div className="right-section">
+                    <img src={logo} className="login-picture" alt="logo"/>
+                </div>
+            </div>
+            </body>
+        </>
+    );
 }
 
 export default App;
