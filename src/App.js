2d750d5370aa0c190d6861629889ae0f09329c41
Matteo F <matteof19@icloud.com>
2024-10-10 15:19:16 +0200

    Body

--- src/App.js
+++ src/App.js
@@ -3,22 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <body>
+        <div className="header">
+          <div className="left-section">
+            <img src={logo} className="App-logo" alt="logo"/>
+          </div>
+          <div className="middle-section">
+
+          </div>
+          <div className="right-section">
+
+          </div>
+        </div>
+        </body>
+      </>
   );
 }
 
