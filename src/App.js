e036d8651a944ed702f9628767c6f1e42fd093d5
AndreNeb <andrenebauer12@outlook.de>
2024-10-10 16:13:28 +0200

    Added Css to topbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-import logo from './pictures/login-picture.jpg';
-import hamburger from './pictures/hamburger-menu.png';
 import './stylesheets/topbar.css';
 
 function App() {
@@ -12,14 +10,14 @@ function App() {
         <body>
         <div className="header">
           <div className="left-section">
-            <img src={hamburger} className="hamburger-menu" alt="Menu"/>
+            <img src="pictures/hamburger-menu.png" className="hamburger-menu" alt="Menu"/>
 
           </div>
           <div className="middle-section">
               <input className= "search-bar" type ="text" placeholder="Suchen" />
           </div>
           <div className="right-section">
-            <img src= {logo} className="login" alt="login"/>
+            <img src= "pictures/login-picture.jpg" className="login" alt="login"/>
           </div>
         </div>
 
