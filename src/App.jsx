aa655a095e28139d3cf251fc344abc15b297ee38
AndreNeb <andrenebauer12@outlook.de>
2024-10-11 19:25:29 +0200

    Piktogramme bei den small widgets richten sich jetzt richtig aus

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+
 import login from './pictures/login-picture.png';
 import logo from './pictures/hamburger-menu.png';
 import language from './pictures/language.png';
@@ -14,99 +17,116 @@ import './stylesheets/leftbar/buttons.css';
 import './stylesheets/general/general.css';
 import './stylesheets/homepage-widgets/small-widgets.css';
 import './stylesheets/homepage-widgets/big-widgets.css';
+import RulesPage from './RulesPage';
+
 
 function App() {
     return (
+        <Router>
+            <>
+                <Routes>
+                    <Route path="/" element={
+                        <body>
+                        <div className="header">
+                            <div className="left-section">
+                                <img src={logo} className="logo" alt="Logo"/>
+                                <div className="topbar-functional-divs">
+                                    <Link to="/RulesPage">
+                                        <button className="Rules-Button">
+                                            <img src={regeln} className="regeln" alt="Regeln"/>
+                                        </button>
 
-        <>
-            <header></header>
-            <body>
-            <div className="header">
-                <div className="left-section">
-                    <img src={logo} className="logo" alt="Logo"/>
-                    <img src={regeln} className="regeln" alt="Regeln"/>
-                    <img src={info} className="info" alt="Info"/>
-                    <img src={support} className="support" alt="Support"/>
-                </div>
-                <div className="middle-section">
-                    <input className="search-bar" type="text" placeholder="Suchen"/>
-                </div>
-                <div className="right-section">
-                    <img src={language} className="language" alt="language"/>
-                    <img src={login} className="login" alt="login"/>
-                </div>
-            </div>
+                                    </Link>
+                                </div>
+                                <img src={info} className="info" alt="Info"/>
+                                <img src={support} className="support" alt="Support"/>
+                            </div>
+                            <div className="middle-section">
+                                <input className="search-bar" type="text" placeholder="Suchen..."/>
+                            </div>
+                            <div className="right-section">
+                                <img src={language} className="language" alt="language"/>
+                                <img src={login} className="login" alt="login"/>
+                            </div>
+                        </div>
 
-            <div className="small-widgets-outer-div">
-                <div className="general-small-widget-layout small-widget1">
-                    <div className="small-widgets-color small-widget1-color-patienten">
-                        &nbsp;
-                    </div>
-                    <div className="small-widgets-text">
-                        <div className="small-widgets-text-top">Patienten</div>
-                        <div className="small-widgets-text-middle">4.500</div>
-                        <div className="small-widgets-text-bottom">&uarr; &#43;8,5 &#037;</div>
-                    </div>
-                    <div>
-                        <img src={patienten} className="small-widgets-picture-layout" alt="Patienten"/>
-                    </div>
-                </div>
-                <div className="general-small-widget-layout small-widget2">
-                    <div className="small-widgets-color small-widget2-color-patienten">
-                        &nbsp;
-                    </div>
-                    <div className="small-widgets-text">
-                        <div className="small-widgets-text-top">OP - Auslastung</div>
-                        <div className="small-widgets-text-middle">70 &#037;</div>
-                        <div className="small-widgets-text-bottom">8 / 9 Belegt</div>
-                    </div>
-                    <div>
-                        <img src={maske} className="small-widgets-picture-layout" alt="OP - Maske"/>
-                    </div>
-                </div>
-                <div className="general-small-widget-layout small-widget3">
-                    <div className="small-widgets-color small-widget3-color-patienten">
-                        &nbsp;
-                    </div>
-                    <div className="small-widgets-text">
-                        <div className="small-widgets-text-top">Narkosen</div>
-                        <div className="small-widgets-text-middle">400</div>
-                        <div className="small-widgets-text-bottom">&darr; 4,5 &#037;</div>
-                    </div>
-                    <div>
-                        <img src={spritze} className="small-widgets-picture-layout" alt="Spritze"/>
-                    </div>
-                </div>
-                <div className="general-small-widget-layout small-widget4">
-                    <div className="small-widgets-color small-widget4-color-patienten">
-                        &nbsp;
-                    </div>
-                    <div className="small-widgets-text">
-                        <div className="small-widgets-text-top">Wartezimmer</div>
-                        <div className="small-widgets-text-middle">9 Patienten</div>
-                        <div className="small-widgets-text-bottom">Gut belegt</div>
-                    </div>
-                    <div>
-                        <img src={wartezimmer} className="small-widgets-picture-layout wartezimmer-picture-layout" alt="Wartezimmer"/>
-                    </div>
-                </div>
-            </div>
+                        <div className="small-widgets-outer-div">
+                            <div className="general-small-widget-layout small-widget1">
+                                <div className="small-widgets-color small-widget1-color-patienten">
+                                    &nbsp;
+                                </div>
+                                <div className="small-widgets-text">
+                                    <div className="small-widgets-text-top">Patienten</div>
+                                    <div className="small-widgets-text-middle">4.500</div>
+                                    <div className="small-widgets-text-bottom">&uarr; &#43;8,5 &#037;</div>
+                                </div>
+                                <div>
+                                    <img src={patienten} className="small-widgets-picture-layout" alt="Patienten"/>
+                                </div>
+                            </div>
+                            <div className="general-small-widget-layout small-widget2">
+                                <div className="small-widgets-color small-widget2-color-patienten">
+                                    &nbsp;
+                                </div>
+                                <div className="small-widgets-text">
+                                    <div className="small-widgets-text-top">OP - Auslastung</div>
+                                    <div className="small-widgets-text-middle">70 &#037;</div>
+                                    <div className="small-widgets-text-bottom">8 / 9 Belegt</div>
+                                </div>
+                                <div>
+                                    <img src={maske} className="small-widgets-picture-layout" alt="OP - Maske"/>
+                                </div>
+                            </div>
+                            <div className="general-small-widget-layout small-widget3">
+                                <div className="small-widgets-color small-widget3-color-patienten">
+                                    &nbsp;
+                                </div>
+                                <div className="small-widgets-text">
+                                    <div className="small-widgets-text-top">Narkosen</div>
+                                    <div className="small-widgets-text-middle">400</div>
+                                    <div className="small-widgets-text-bottom">&darr; 4,5 &#037;</div>
+                                </div>
+                                <div>
+                                    <img src={spritze} className="small-widgets-picture-layout" alt="Spritze"/>
+                                </div>
+                            </div>
+                            <div className="general-small-widget-layout small-widget4">
+                                <div className="small-widgets-color small-widget4-color-patienten">
+                                    &nbsp;
+                                </div>
+                                <div className="small-widgets-text">
+                                    <div className="small-widgets-text-top">Wartezimmer</div>
+                                    <div className="small-widgets-text-middle">9 Patienten</div>
+                                    <div className="small-widgets-text-bottom">Gut belegt</div>
+                                </div>
+                                <div>
+                                    <img src={wartezimmer}
+                                         className="small-widgets-picture-layout wartezimmer-picture-layout"
+                                         alt="Wartezimmer"/>
+                                </div>
+                            </div>
+                        </div>
 
-            <div className="big-widgets-general">
+                        <div className="big-widgets-general">
 
-                <div className="big-widget">Calendar</div>
+                            <div className="big-widget">Calendar</div>
 
-                <div className="big-widget">Mail</div>
-            </div>
-            <div className="big-widgets-general">
+                            <div className="big-widget">Mail</div>
+                        </div>
+                        <div className="big-widgets-general">
 
-                <div className="big-widget">Calendar</div>
+                            <div className="big-widget">Calendar</div>
 
-                <div className="big-widget">Mail</div>
-            </div>
-            </body>
-        </>
+                            <div className="big-widget">Mail</div>
+                        </div>
+                        </body>}/>
 
+                    <Route path="/rulesPage" element={<RulesPage/>}/>
+
+                </Routes>
+            </>
+
+        </Router>
     );
 }
 
