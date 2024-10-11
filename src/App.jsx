bb0e4ab5df39a8b08257f9c829c5874c4a6d95d8
AndreNeb <andrenebauer12@outlook.de>
2024-10-11 16:18:15 +0200

    Edited small widgets

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import language from './pictures/language.png';
 import regeln from './pictures/regeln.png';
 import info from './pictures/info.png';
 import support from './pictures/support.png';
+import patienten from './pictures/patienten.png';
 import './stylesheets/topbar/topbar.css';
 import './stylesheets/leftbar/leftbar.css';
 import './stylesheets/leftbar/buttons.css';
@@ -33,45 +34,48 @@ function App() {
                 </div>
             </div>
 
-            <div className="small-widgets-general">
-                <div className="small-widget1">
-                    <div className="small-widget1-color-patienten">
+            <div className="small-widgets-outer-div">
+                <div className="general-small-widget-layout small-widget1">
+                    <div className="small-widgets-color small-widget1-color-patienten">
                         &nbsp;
                     </div>
-                    <div className="small-widget1-text-patienten">
-                        <div>Patienten</div>
-                        <div>4500</div>
-                        <div>&#43;8.5 &#037;</div>
+                    <div className="small-widgets-text">
+                        <div className="small-widgets-text-top">Patienten</div>
+                        <div className="small-widgets-text-middle">4.500</div>
+                        <div className="small-widgets-text-bottom">&#43;8,5 &#037;</div>
+                    </div>
+                    <div>
+                        <img src={patienten} className="small-widgets-picture-layout" alt="Patienten"/>
                     </div>
                 </div>
-                <div className="small-widget2">
-                    <div className="small-widget2-color-patienten">
+                <div className="general-small-widget-layout small-widget2">
+                    <div className="small-widgets-color small-widget2-color-patienten">
                         &nbsp;
                     </div>
-                    <div className="small-widget2-text-patienten">
-                        <div>OP - Auslastung</div>
-                        <div>70 &#037;</div>
-                        <div>8 / 9 Belegt</div>
+                    <div className="small-widgets-text">
+                        <div className="small-widgets-text-top">OP - Auslastung</div>
+                        <div className="small-widgets-text-middle">70 &#037;</div>
+                        <div className="small-widgets-text-bottom">8 / 9 Belegt</div>
                     </div>
                 </div>
-                <div className="small-widget3">
-                    <div className="small-widget3-color-patienten">
+                <div className="general-small-widget-layout small-widget3">
+                    <div className="small-widgets-color small-widget3-color-patienten">
                         &nbsp;
                     </div>
-                    <div className="small-widget3-text-patienten">
-                        <div>Narkosen</div>
-                        <div>400</div>
-                        <div>4.5 &#037;</div>
+                    <div className="small-widgets-text">
+                        <div className="small-widgets-text-top">Narkosen</div>
+                        <div className="small-widgets-text-middle">400</div>
+                        <div className="small-widgets-text-bottom">4,5 &#037;</div>
                     </div>
                 </div>
-                <div className="small-widget4">
-                    <div className="small-widget4-color-patienten">
+                <div className="general-small-widget-layout small-widget4">
+                    <div className="small-widgets-color small-widget4-color-patienten">
                         &nbsp;
                     </div>
-                    <div className="small-widget4-text-patienten">
-                        <div>Wartezimmer</div>
-                        <div>11 Patienten</div>
-                        <div>Gut belegt</div>
+                    <div className="small-widgets-text">
+                        <div className="small-widgets-text-top">Wartezimmer</div>
+                        <div className="small-widgets-text-middle">11 Patienten</div>
+                        <div className="small-widgets-text-bottom">Gut belegt</div>
 
                     </div>
                 </div>
