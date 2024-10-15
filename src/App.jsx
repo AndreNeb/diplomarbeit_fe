93629a384b4a58349de8c66c207da1ec981b42c2
AndreNeb <andrenebauer12@outlook.de>
2024-10-15 21:51:32 +0200

    Topbar angepasst, min width für Topbar, Buttons neu designed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import {useTranslation} from "react-i18next";
 
 
 import React, {useState} from "react";
-import {BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Link, useNavigate} from "react-router-dom";
 
 import login from './pictures/login-picture.png';
-import logo from './pictures/hamburger-menu.png';
 import language from './pictures/language.png';
 import regeln from './pictures/regeln.png';
 import info from './pictures/info.png';
@@ -18,6 +17,7 @@ import wartezimmer from './pictures/wartezimmer.png';
 import document from './pictures/document.png';
 import moon from './pictures/moon.png';
 import sun from './pictures/sun.png';
+import apple from './pictures/apple.png';
 import './stylesheets/topbar/topbar.css';
 import './stylesheets/leftbar/leftbar.css';
 import './stylesheets/leftbar/buttons.css';
@@ -46,131 +46,168 @@ function App() {
         setDarklight(!showdarklight);
     };
 
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+
+
     return (
         <Router>
             <>
                 <Routes>
                     <Route path="/" element={
-                        <body className="homepage">
-                        <div className="header">
-                            <div className="left-section">
-                                <Link to="/">
-                                    <img src={logo} className="logo" alt="Logo"/>
-                                </Link>
+                        <div className="homepage">
+                            <div className="header">
+                                <div className="left-section">
+                                    <Link to="/">
+                                        <img src={apple} className="logo" alt={t('logo')}/>
+                                    </Link>
 
-                                <div className="topbar-functional-divs">
+                                    <div className="topbar-functional-divs">
+                                        <Link to="/RulesPage">
+                                            <img src={regeln} className="icons" alt={t('rules')}/>
+                                        </Link>
+
+                                        <Link to="/DocumentPage">
+                                            <img src={document} className="icons" alt={t('documents')}/>
+                                        </Link>
+
+                                        <Link to="/InfoPage">
+                                            <img src={info} className="icons" alt={t('info')}/>
+                                        </Link>
+
+                                        <Link to="/SupportPage">
+                                            <img src={support} className="icons" alt={t('support')}/>
+                                        </Link>
+
+                                    </div>
+                                </div>
+                                <div className="middle-section">
+                                    <input className="search-bar" type="text" placeholder={t('searchbar-placeholder')}/>
+                                </div>
+                                <div className="right-section">
+                                    {showdarklight ? (
+                                        <img onClick={Mode} src={moon} className="icons mode" alt="Mode"/>
+                                    ) : (
+                                        <img onClick={Mode} src={sun} className="icons mode" alt="Mode"/>
+                                    )}
+                                    <img src={language} className="icons" alt={t('language')}/>
+                                    <img src={login} className="icons" alt="login"/>
+                                </div>
+                            </div>
+
+                            <div className="small-widgets-outer-div">
+                                <div className="general-small-widget-layout small-widget1">
+                                    <div className="small-widgets-color small-widget1-color-patienten">
+                                        &nbsp;
+                                    </div>
+                                    <div className="small-widgets-text">
+                                        <div className="small-widgets-text-top">{t('patient')}</div>
+                                        <div className="small-widgets-text-middle">4.500</div>
+                                        <div className="small-widgets-text-bottom">&uarr; &#43;8,5 &#037;</div>
+                                    </div>
+                                    <div>
+                                        <img src={patienten} className="small-widgets-picture-layout" alt="Patienten"/>
+                                    </div>
+                                </div>
+                                <div className="general-small-widget-layout small-widget2">
+                                    <div className="small-widgets-color small-widget2-color-patienten">
+                                        &nbsp;
+                                    </div>
+                                    <div className="small-widgets-text">
+                                        <div className="small-widgets-text-top">{t('op-workload')}</div>
+                                        <div className="small-widgets-text-middle">70 &#037;</div>
+                                        <div className="small-widgets-text-bottom">8 / 9 {t('unavailable')}</div>
+                                    </div>
+                                    <div>
+                                        <img src={maske} className="small-widgets-picture-layout" alt="OP - Maske"/>
+                                    </div>
+                                </div>
+                                <div className="general-small-widget-layout small-widget3">
+                                    <div className="small-widgets-color small-widget3-color-patienten">
+                                        &nbsp;
+                                    </div>
+                                    <div className="small-widgets-text">
+                                        <div className="small-widgets-text-top">{t('anesthesia')}</div>
+                                        <div className="small-widgets-text-middle">400</div>
+                                        <div className="small-widgets-text-bottom">&darr; 4,5 &#037;</div>
+                                    </div>
+                                    <div>
+                                        <img src={spritze} className="small-widgets-picture-layout" alt="Spritze"/>
+                                    </div>
+                                </div>
+                                <div className="general-small-widget-layout small-widget4">
+                                    <div className="small-widgets-color small-widget4-color-patienten">
+                                        &nbsp;
+                                    </div>
+                                    <div className="small-widgets-text">
+                                        <div className="small-widgets-text-top">{t('waiting-room')}</div>
+                                        <div className="small-widgets-text-middle">9 {t('patients')}</div>
+                                        <div className="small-widgets-text-bottom">  {t('waiting-room-usage')}</div>
+                                    </div>
+                                    <div>
+                                        <img src={wartezimmer}
+                                             className="small-widgets-picture-layout wartezimmer-picture-layout"
+                                             alt="Wartezimmer"/>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="big-widgets-general">
+
+                                <div className="big-widget">Calendar</div>
+                                <div className="big-widget">Arbeitszeit</div>
+
+
+                            </div>
+
+                            <div className="copyright-text-outer-div">
+                                <div className="copyright-text-inner-div">
+                                    <div>&copy; 2024</div>
+                                    <br/>
+                                    Feichtenschlager,
+                                    Nebauer,
+                                    Würcher
+                                    <div className="htl">HTBLuVA Salzburg</div>
+                                    <br/>
+                                </div>
+                            </div>
+
+
+                            <div className={`hamburger-menu ${isOpen ? 'open' : ''}`}>
+                                <div className="hamburger-menu-div">
+                                    <button className="hamburger-button" onClick={toggleMenu}>
+                                        {/* Hamburger Icon */}
+                                        <span className="hamburger-icon"></span>
+                                        <span className="hamburger-icon"></span>
+                                        <span className="hamburger-icon"></span>
+                                    </button>
+
+                                    <img src={apple} className="logo-in-hamburger-menu" alt="Logo"/>
+                                </div>
+                            </div>
+
+                            {/* Das Menü, das den gesamten Bildschirm abdeckt */}
+                            <div className={`menu-overlay ${isOpen ? 'open' : ''}`}>
+                                <div className="menu-items">
+
+
+                                    <a href="#contact" onClick={toggleMenu}>Home</a>
                                     <Link to="/RulesPage">
-                                        <img src={regeln} className="icons-left" alt="Rules"/>
+                                        <a href="#about" onClick={toggleMenu}>Regeln</a>
                                     </Link>
-
-
-                                    <Link to="/DocumentPage">
-                                        <img src={document} className="icons-left" alt="Document"/>
-                                    </Link>
-
-
-                                    <Link to="/InfoPage">
-                                        <img src={info} className="icons-left" alt="Info"/>
-                                    </Link>
-
-
-                                    <Link to="/SupportPage" className="icon-link">
-                                        <img src={support} className="icons-left" alt="Support"/>
-                                    </Link>
-
+                                    <a href="#about" onClick={toggleMenu}>Dokumente</a>
+                                    <a href="#services" onClick={toggleMenu}>Info</a>
+                                    <a href="#contact" onClick={toggleMenu}>Support</a>
+                                    <a href="#contact" onClick={toggleMenu}>Logout</a>
                                 </div>
                             </div>
-                            <div className="middle-section">
-                                <input className="search-bar" type="text" placeholder={t('searchbar-placeholder')}/>
-                            </div>
-                            <div className="right-section">
-                                {showdarklight ? (
-                                    <img onClick={Mode} src={moon} className="mode" alt="Mode"/>
-                                ) : (
-                                    <img onClick={Mode} src={sun} className="mode" alt="Mode"/>
-                                )}
-                                <img src={language} className="icons-right" alt={t('language')}/>
-                                <img src={login} className="login" alt="login"/>
-                            </div>
-                        </div>
-
-                        <div className="small-widgets-outer-div">
-                            <div className="general-small-widget-layout small-widget1">
-                                <div className="small-widgets-color small-widget1-color-patienten">
-                                    &nbsp;
-                                </div>
-                                <div className="small-widgets-text">
-                                    <div className="small-widgets-text-top">{t('patient')}</div>
-                                    <div className="small-widgets-text-middle">4.500</div>
-                                    <div className="small-widgets-text-bottom">&uarr; &#43;8,5 &#037;</div>
-                                </div>
-                                <div>
-                                    <img src={patienten} className="small-widgets-picture-layout" alt="Patienten"/>
-                                </div>
-                            </div>
-                            <div className="general-small-widget-layout small-widget2">
-                                <div className="small-widgets-color small-widget2-color-patienten">
-                                    &nbsp;
-                                </div>
-                                <div className="small-widgets-text">
-                                    <div className="small-widgets-text-top">{t('op-workload')}</div>
-                                    <div className="small-widgets-text-middle">70 &#037;</div>
-                                    <div className="small-widgets-text-bottom">8 / 9 {t('unavailable')}</div>
-                                </div>
-                                <div>
-                                    <img src={maske} className="small-widgets-picture-layout" alt="OP - Maske"/>
-                                </div>
-                            </div>
-                            <div className="general-small-widget-layout small-widget3">
-                                <div className="small-widgets-color small-widget3-color-patienten">
-                                    &nbsp;
-                                </div>
-                                <div className="small-widgets-text">
-                                    <div className="small-widgets-text-top">{t('anesthesia')}</div>
-                                    <div className="small-widgets-text-middle">400</div>
-                                    <div className="small-widgets-text-bottom">&darr; 4,5 &#037;</div>
-                                </div>
-                                <div>
-                                    <img src={spritze} className="small-widgets-picture-layout" alt="Spritze"/>
-                                </div>
-                            </div>
-                            <div className="general-small-widget-layout small-widget4">
-                                <div className="small-widgets-color small-widget4-color-patienten">
-                                    &nbsp;
-                                </div>
-                                <div className="small-widgets-text">
-                                    <div className="small-widgets-text-top">{t('waiting-room')}</div>
-                                    <div className="small-widgets-text-middle">9 {t('patients')}</div>
-                                    <div className="small-widgets-text-bottom">  {t('waiting-room-usage')}</div>
-                                </div>
-                                <div>
-                                    <img src={wartezimmer}
-                                         className="small-widgets-picture-layout wartezimmer-picture-layout"
-                                         alt="Wartezimmer"/>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="big-widgets-general">
-
-                            <div className="big-widget">Calendar</div>
-                            <div className="big-widget">Arbeitszeit</div>
 
 
-                        </div>
-
-                        <div className="copyright-text-outer-div">
-                            <div className="copyright-text-inner-div">
-                                <div>&copy; 2024</div>
-                                <br/>
-                                Feichtenschlager,
-                                Nebauer,
-                                Würcher
-                                <div className="htl">HTBLuVA Salzburg</div>
-                            <br/>
-                            </div>
-                        </div>
-                        </body>}/>
+                        </div>}/>
 
                     <Route path="/rulesPage" element={<RulesPage/>}/>
                     <Route path="/documentPage" element={<DocumentPage/>}/>
@@ -181,6 +218,7 @@ function App() {
 
         </Router>
     );
+
 }
 
 export default App;
