40b6b366db9fde639b6ef5b36b75e3b5be7f7de1
AndreNeb <andrenebauer12@outlook.de>
2024-10-20 11:57:06 +0200

    Schriftzug anstatt Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
              style={{backgroundColor: darkMode ? 'black' : 'white', transition: "background-color 1.1s ease"}}>
             <div className="left-section">
                 <Link to="/">
-                    <span style={{color: darkMode ? "white": "black", fontSize: "26px", marginLeft: "30px"}}>N A R K O</span>
+                    <span style={{color: darkMode ? "white": "black", fontSize: "26px", marginLeft: "30px", fontWeight: "600"}}>N A R K O</span>
                 </Link>
 
                 <div className="topbar-functional-divs">
