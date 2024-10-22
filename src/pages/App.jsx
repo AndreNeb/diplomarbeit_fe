ac56fe4a65dac694bb93dc32f25bd3c6047f02df
AndreNeb <andrenebauer12@outlook.de>
2024-10-22 08:42:36 +0200

    Seitenübergangsanimationen erstellt

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,34 +1,53 @@
-import '../components/i18n'
-
-import {BrowserRouter as Router, Route, Routes, } from "react-router-dom";
-
+import React, { useState, useEffect } from 'react';
+import '../components/i18n';
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import RulesPage from './RulesPage';
 import SupportPage from "./SupportPage";
 import InfoPage from "./InfoPage";
 import DocumentPage from "./DocumentPage";
 import LogInOutPage from "./LogInOutPage";
 import Home from "./Home";
-import {DarkModeProvider} from "../components/DarkModeContext";
+import { DarkModeProvider } from "../components/DarkModeContext";
+import LoadingPage from './LoadingPage'; // Importiere die Ladekomponente
 
+const AppRoutes = () => {
+    const location = useLocation(); // useLocation kann hier verwendet werden, da es innerhalb des Routers ist
 
-function App() {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 850);
+
+        return () => clearTimeout(timer);
+    }, [location]);
 
     return (
-        <DarkModeProvider>
-        <Router>
-            <>
+        <>
+            {loading ? (
+                <LoadingPage />
+            ) : (
                 <Routes>
-                    <Route path="/" element={<Home/>} />
-                    <Route path="/rulesPage" element={<RulesPage/>}/>
-                    <Route path="/documentPage" element={<DocumentPage/>}/>
-                    <Route path="/infoPage" element={<InfoPage/>}/>
-                    <Route path="/supportPage" element={<SupportPage/>}/>
-                    <Route path="/loginoutpage" element={<LogInOutPage/>}/>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/rulesPage" element={<RulesPage />} />
+                    <Route path="/documentPage" element={<DocumentPage />} />
+                    <Route path="/infoPage" element={<InfoPage />} />
+                    <Route path="/supportPage" element={<SupportPage />} />
+                    <Route path="/loginoutpage" element={<LogInOutPage />} />
                 </Routes>
+            )}
+        </>
+    );
+};
 
-            </>
-
-        </Router>
+function App() {
+    return (
+        <DarkModeProvider>
+            <Router>
+                <AppRoutes />
+            </Router>
         </DarkModeProvider>
     );
 }
